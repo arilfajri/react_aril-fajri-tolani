10129cd7f2adc0b8bc4499b8c7491316983c124d
arilfajri <arielmunchen60@gmail.com>
2023-03-09 23:48:49 +0700

    Praktikum – React Fundamental

---
+++ 14_React Fundamental/Praktikum/create-product/src/App.js
@@ -0,0 +1,20 @@
+import Header from "./components/header/Header";
+import Form from "./components/form/Form";
+import ButtonForm from "./components/button/ButtonForm";
+import Table from "./components/table/Table";
+import ButtonTable from "./components/button/ButtonTable";
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Form />
+      <ButtonForm />
+      <Table />
+      <ButtonTable />
+    </>
+  );
+}
+
+export default App;
