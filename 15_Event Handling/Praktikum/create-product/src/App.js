257c2bc30486aca045e82a25e4dedc295455e21f
arilfajri <arielmunchen60@gmail.com>
2023-03-16 13:23:42 +0700

    Praktikum Event Handling

--- 15_Event Handling/Praktikum/create-product/src/App.js
+++ 15_Event Handling/Praktikum/create-product/src/App.js
@@ -4,22 +4,9 @@ import ButtonForm from "./components/button/ButtonForm";
 import Table from "./components/table/Table";
 import ButtonTable from "./components/button/ButtonTable";
 import './App.css';
-import React, { useState } from 'react'
 import ButtonRandom from "./components/button/ButtonRandom";
-import ButtonChange from "./components/button/ButtonChange";
 
-
-
-function TodoList() {
-  const [todos, setTodos] = useState([]);
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const newTodo = event.target.elements.todo.value;
-    setTodos([...todos, newTodo]);
-    event.target.reset();
-  };
-
+function App() {
   return (
     <>
       <Header />
@@ -32,5 +19,5 @@ function TodoList() {
   );
 }
 
-export default TodoList;
+export default App;
 
